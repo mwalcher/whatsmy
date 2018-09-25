0ecb8238e2c26ecc1130315ad6aea0f23ce72805
Matthew Walcher <mwalcher@live.ca>
2018-09-24 22:08:02 -0400

    set up base utilities and colours

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/images/whatsmy-logo.svg" alt="WhatsMy Logo">
-  </div>
+    <div id="app">
+        <img class="logo" src="./assets/images/whatsmy-logo.svg" alt="WhatsMy Logo">
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+    name: 'app'
 }
 </script>
