2c5d69a81c4c6fd0b6a539931e6d7f7a0e8e4cfe
Matthew Walcher <mwalcher@live.ca>
2018-09-26 22:18:36 -0400

    updated base styles + set up header menu button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,13 @@
     <header>
         <div class="container">
             <img class="logo" src="../assets/images/whatsmy-logo.svg" alt="WhatsMy Logo">
+            <button id="menuToggle">
+                <span class="menuLabel">Menu</span>
+                <span class="menuIcon">
+                    <span></span>
+                    <span></span>
+                </span>
+            </button>
         </div>
     </header>
 </template>
