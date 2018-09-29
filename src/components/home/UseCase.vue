47ece53bae0bad869a503f80b57049121e8f4877
Matthew Walcher <mwalcher@live.ca>
2018-09-28 22:41:02 -0400

    added home use case section

---
+++ src/components/home/UseCase.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="use-case">
+        <div class="container">
+            <div class="use-case-content">
+                <div class="image-container">
+                    <img
+                        :src="require(`@/assets/images/${imageSrc}`)"
+                        :alt="imageAlt"
+                    />
+                </div>
+
+                <div class="content-container">
+                    <h2 class="subtitle">{{title}}</h2>
+                    <h3 class="option-title h2">
+                        <span class="overlap">{{optionsTitleStart}}</span>
+                        <button>{{optionName}}</button>
+                        <span>{{optionsTitleEnd}}</span>
+                    </h3>
+                    <p>{{optionContent}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "UseCase",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    optionsTitleStart: {
+      type: String,
+      required: true
+    },
+    optionsTitleEnd: {
+      type: String,
+      required: true
+    },
+    optionName: {
+      type: String,
+      required: true
+    },
+    optionContent: {
+      type: String,
+      required: true
+    },
+    imageSrc: {
+      type: String,
+      required: true
+    },
+    imageAlt: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,20 @@
             buttonLink="/"
             buttonText="Get Started"
             buttonTitle="Get Started"
-            imageSrc="placeholder.png"
+            imageSrc="placeholder-hero.png"
             imageAlt="Hero Image"
         />
         <LeadContent
             text="We get it, selling your home and car is hard. WhatsMy will provide you with this knowledge on a monthly basis, so when it’s time for you to buy or trade in your vehicle, alll you have to do is open your WhatsMy app and start using it."
+        />
+        <UseCase
+            title="Use Cases"
+            optionsTitleStart="WhatsMy"
+            optionsTitleEnd="worth?"
+            optionName="house"
+            optionContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse eu."
+            imageSrc="placeholder-use-case.png"
+            imageAlt="Use Case Image"
         />
         <SimpleCTA
             title="Get Started"
@@ -24,6 +33,7 @@
 <script>
 import Hero from "@/components/home/Hero.vue";
 import LeadContent from "@/components/home/LeadContent.vue";
+import UseCase from "@/components/home/UseCase.vue";
 import SimpleCTA from "@/components/common/SimpleCTA.vue";
 
 export default {
@@ -31,6 +41,7 @@ export default {
   components: {
     Hero,
     LeadContent,
+    UseCase,
     SimpleCTA
   }
 };
