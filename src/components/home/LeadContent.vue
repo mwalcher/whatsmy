48926a1a1cb972459fc7edcb6c1f608d352478b2
Matthew Walcher <mwalcher@live.ca>
2018-09-28 21:43:22 -0400

    added home lead content component

---
+++ src/components/home/LeadContent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="lead-content">
+        <div class="container">
+            <div class="content-container">
+                <p class="leadText">{{text}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "LeadContent",
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,9 @@
             imageSrc="placeholder.png"
             imageAlt="Hero Image"
         />
+        <LeadContent
+            text="We get it, selling your home and car is hard. WhatsMy will provide you with this knowledge on a monthly basis, so when it’s time for you to buy or trade in your vehicle, alll you have to do is open your WhatsMy app and start using it."
+        />
         <SimpleCTA
             title="Get Started"
             buttonLink="/"
@@ -20,12 +23,14 @@
 
 <script>
 import Hero from "@/components/home/Hero.vue";
+import LeadContent from "@/components/home/LeadContent.vue";
 import SimpleCTA from "@/components/common/SimpleCTA.vue";
 
 export default {
   name: "home",
   components: {
     Hero,
+    LeadContent,
     SimpleCTA
   }
 };
