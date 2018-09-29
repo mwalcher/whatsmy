56c14a2af1b479ea3de80e88a6607939d84343ac
Matthew Walcher <mwalcher@live.ca>
2018-09-28 21:28:11 -0400

    added home hero component

---
+++ src/components/home/Hero.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="hero">
+        <div class="container">
+            <div class="hero-content">
+                <div class="content-container">
+                    <h1>
+                        <span>{{titleTop}}</span>
+                        <span>{{titleBottom}}</span>
+                    </h1>
+                    <router-link
+                        :to="buttonLink"
+                        :title="buttonTitle"
+                        class="button"
+                    >
+                        {{buttonText}}
+                    </router-link>
+                </div>
+
+                <div class="hero-gradient"/>
+
+                <div class="image-container">
+                    <img
+                        :src="require(`@/assets/images/${imageSrc}`)"
+                        :alt="imageAlt"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHero",
+  props: {
+    titleTop: {
+      type: String,
+      required: true
+    },
+    titleBottom: {
+      type: String,
+      required: true
+    },
+    buttonLink: {
+      type: String,
+      required: true
+    },
+    buttonText: {
+      type: String,
+      required: true
+    },
+    buttonTitle: {
+      type: String,
+      required: true
+    },
+    imageSrc: {
+      type: String,
+      required: true
+    },
+    imageAlt: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,31 @@
 <template>
-    <div class="home">
+    <main class="home">
+        <Hero
+            titleTop="WhatsMy:"
+            titleBottom="A Digital App"
+            buttonLink="/"
+            buttonText="Get Started"
+            buttonTitle="Get Started"
+            imageSrc="placeholder.png"
+            imageAlt="Hero Image"
+        />
         <SimpleCTA
             title="Get Started"
             buttonLink="/"
             buttonText="Get Started"
             buttonTitle="Get Started"
         />
-    </div>
+    </main>
 </template>
 
 <script>
+import Hero from "@/components/home/Hero.vue";
 import SimpleCTA from "@/components/common/SimpleCTA.vue";
 
 export default {
   name: "home",
   components: {
+    Hero,
     SimpleCTA
   }
 };
