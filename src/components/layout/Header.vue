7c2b64cebccb8413795e0b358e0e5f4107c41053
Matthew Walcher <mwalcher@live.ca>
2018-10-03 23:22:37 -0400

    added base navigation animation

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header>
-        <div class="container">
+        <div class="header-container container">
             <router-link to="/" title="Home">
                 <img class="logo" :src="require('@/assets/images/whatsmy-logo.svg')" alt="WhatsMy Logo">
             </router-link>
@@ -12,31 +12,35 @@
                 </span>
             </button>
         </div>
-        <nav class="navigation">
-            <ul class="main-navigation">
-                <li>
-                    <router-link to="/" title="Hello">Hello</router-link>
-                </li>
-                <li>
-                    <router-link to="/" title="How It Works">How It Works</router-link>
-                </li>
-                <li>
-                    <router-link to="/" title="Our Story">Our Story</router-link>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <router-link to="/" title="Get In Touch">Get In Touch</router-link>
-                </li>
-                <li>
-                    <router-link to="/" title="FAQ">FAQ</router-link>
-                </li>
-                <li>
-                    <router-link to="/" title="Privacy Policy">Privacy Policy</router-link>
-                </li>
-            </ul>
-            <p class="copyright">© 2018 Mobials Inc.</p>
-        </nav>
+        <div class="navigation">
+            <div class="container">
+                <nav>
+                    <ul class="main-navigation">
+                        <li>
+                            <router-link to="/" title="Hello">Hello</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/" title="How It Works">How It Works</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/" title="Our Story">Our Story</router-link>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <router-link to="/" title="Get In Touch">Get In Touch</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/" title="FAQ">FAQ</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/" title="Privacy Policy">Privacy Policy</router-link>
+                        </li>
+                    </ul>
+                    <p class="copyright">© 2018 Mobials Inc.</p>
+                </nav>
+            </div>
+        </div>
     </header>
 </template>
 
