48d622b72be05baa1962eb74323ddd8723d75ce3
Matthew Walcher <mwalcher@live.ca>
2018-10-06 15:14:38 -0400

    set up base privacy page and simple hero component

---
+++ src/components/common/SimpleHero.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="simple-hero">
+        <div class="container">
+            <h1 v-if="title">{{title}}</h1>
+            <p v-if="content">{{content}}</p>
+            <router-link
+                v-if="buttonLink && buttonText && buttonTitle"
+                :to="buttonLink"
+                :title="buttonTitle"
+                class="button"
+            >
+                {{buttonText}}
+            </router-link>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: "SimpleHero",
+  props: {
+    title: String,
+    content: String,
+    buttonLink: String,
+    buttonText: String,
+    buttonTitle: String
+  }
+};
+</script>
---
+++ src/views/Privacy.vue
@@ -0,0 +1,30 @@
+<template>
+    <main class="privacy">
+        <SimpleHero
+            title="Privacy Policy"
+            content="This Privacy Policy sets out Credsii’s approach to the collection, use and disclosure of personal info in connection with our business in Canada, including but not limited to personal information collected, used and disclosed when you access or use our Website."
+        />
+    </main>
+</template>
+
+<script>
+import SimpleHero from "@/components/common/SimpleHero.vue";
+
+export default {
+  name: "privacy",
+  components: {
+    SimpleHero
+  },
+  data() {
+    return {
+      sections: [
+        {
+          title: "Everything at a glance.",
+          content:
+            "Lorem ipsum dolor sit amet, consec adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+        }
+      ]
+    };
+  }
+};
+</script>
