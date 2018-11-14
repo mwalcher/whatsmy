d2171d86c5587311a3b64ebfdfae42d01e37b4ea
Matthew Walcher <mwalcher@live.ca>
2018-11-13 23:46:02 -0500

    updated responsive hero styles for our story hero

--- src/components/our-story/Hero.vue
+++ src/components/our-story/Hero.vue
@@ -3,44 +3,46 @@
         <div class="container">
             <div class="hero-content">
                 <div class="content-container">
-                    <h1>
-                        <span>{{titleTop}}</span>
-                        <span>{{titleBottom}}</span>
-                    </h1>
-                    <p class="leadText">{{content}}</p>
-                    <router-link
-                        exact
-                        :to="buttonLink"
-                        :title="buttonTitle"
-                        class="button"
-                    >
-                        {{buttonText}}
-                    </router-link>
+                    <div class="content">
+                        <h1>
+                            <span>{{titleTop}}</span>
+                            <span>{{titleBottom}}</span>
+                        </h1>
+                        <p class="leadText">{{content}}</p>
+                        <router-link
+                            exact
+                            :to="buttonLink"
+                            :title="buttonTitle"
+                            class="button"
+                        >
+                            {{buttonText}}
+                        </router-link>
+                    </div>
+
+                    <nav class="page-navigation">
+                        <ul>
+                            <li
+                                v-for="navItem in pageNavigation"
+                                :key="navItem.anchor"
+                            >
+                                <a
+                                    :href="`#${navItem.anchor}`"
+                                    :title="navItem.title"
+                                    class="line-hover"
+                                >{{navItem.title}}</a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
 
-                <nav class="page-navigation">
-                    <ul>
-                        <li
-                            v-for="navItem in pageNavigation"
-                            :key="navItem.anchor"
-                        >
-                            <a
-                                :href="`#${navItem.anchor}`"
-                                :title="navItem.title"
-                                class="line-hover"
-                            >{{navItem.title}}</a>
-                        </li>
-                    </ul>
-                </nav>
+                <div class="image-container">
+                    <img
+                        :src="require(`@/assets/images/${imageSrc}`)"
+                        :alt="imageAlt"
+                    />
+                    <div class="hero-gradient"/>
+                </div>
             </div>
-        </div>
-
-        <div class="image-container">
-            <img
-                :src="require(`@/assets/images/${imageSrc}`)"
-                :alt="imageAlt"
-            />
-            <div class="hero-gradient"/>
         </div>
     </section>
 </template>
